384b81576eeef5110e659dc9cde5e334315eea6e
FarbodHaselzadeh <farbodh@kth.se>
2021-10-22 09:00:33 +0200

    First welcomepage

--- social_media/basic_app/views.py
+++ social_media/basic_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from basic_app import views
+
+def homepage(request):
+    my_dict={'insert me':""}
+    return render (request,"homepage.html",context=my_dict)
 
 # Create your views here.
